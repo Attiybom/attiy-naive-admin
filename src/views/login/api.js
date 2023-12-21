66a8c836fc5ed775e78599aa77c13fc0b8bb5903
luminous <910667665@qq.com>
2023-12-21 18:31:04 +0800

    feat(register): 实现注册功能

--- src/views/login/api.js
+++ src/views/login/api.js
@@ -2,4 +2,5 @@ import { request } from '@/utils'
 
 export default {
   login: (data) => request.post('/auth/login', data, { noNeedToken: true }),
+  register: (data) => request.post('/auth/register', data, { noNeedToken: true }),
 }
